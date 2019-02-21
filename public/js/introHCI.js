207151b37173def92a2f85ae8ab40b82767569b4
Joycelyn Peng <jjp040@ucsd.edu>
2019-02-21 13:31:05 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(recordLike);
+}
+
+function recordLike(e) {
+	e.preventDefault();
+	ga("create", "UA-134838812-1", "auto");
+	ga("send", "event", "like", "click")
 }
